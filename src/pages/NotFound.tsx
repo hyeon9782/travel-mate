37fe2d337bf2255d19d04ed3c625eadd901fd91a
hyeon9782 <hyeon9782@gmail.com>
2023-07-01 20:21:20 +0900

    feat: NotFound 페이지 개발

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,17 @@
+import styled from "styled-components";
+
 const NotFound = () => {
-  return <div></div>;
+  return <NotFoundBlock>404 Not Found</NotFoundBlock>;
 };
+
+const NotFoundBlock = styled.div`
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 70px);
+  font-size: 4rem;
+  font-weight: bold;
+`;
 
 export default NotFound;
