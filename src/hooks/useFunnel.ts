d897104a10e2a04c89a3d6fc88f5a3a622acf1fa
hyeon9782 <hyeon9782@gmail.com>
2023-07-15 21:58:03 +0900

    fix: useFunnel 에러 해결

--- src/hooks/useFunnel.ts
+++ src/hooks/useFunnel.ts
@@ -1,19 +1,19 @@
 import { useState } from "react";
 
-export function useFunnel() {
-  const [step, setStep] = useState();
-  //   const step = useQueryParam("funnel-step")
+// export function useFunnel() {
+//   const [step, setStep] = useState();
+//   //   const step = useQueryParam("funnel-step")
 
-  const Step = (props) => {
-    return <>{props.children}</>;
-  };
+//   const Step = (props) => {
+//     return <>{props.children}</>;
+//   };
 
-  const Funnel = ({ children }) => {
-    // name이 현재 step 상태와 동일한 Step만 렌더링
-    const targetStep = children.find(
-      (childStep) => childStep.props.name === step
-    );
-    return Object.assign(targetStep, { Step });
-  };
-  return [Funnel, setStep];
-}
+//   const Funnel = ({ children }) => {
+//     // name이 현재 step 상태와 동일한 Step만 렌더링
+//     const targetStep = children.find(
+//       (childStep) => childStep.props.name === step
+//     );
+//     return Object.assign(targetStep, { Step });
+//   };
+//   return [Funnel, setStep];
+// }
