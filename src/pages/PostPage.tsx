3e06f171d5e6801ee3b74172b69a33a522996217
hyeon9782 <hyeon9782@gmail.com>
2023-08-03 02:09:49 +0900

    feat: 상세페이지 컴포넌트 개발

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -13,7 +13,7 @@ const PostPage = () => {
     content: "임시 내용입니다.",
     plan_id: "0",
   });
-  const { title, content, plan_id } = post;
+  const { title, content, plan_id, user_id } = post;
 
   useEffect(() => {
     setPost({ ...postParam });
@@ -23,29 +23,69 @@ const PostPage = () => {
     <PostPageBlock>
       <PrevStep onPrev={() => navigate(-1)} />
       <PostTitle>{title}</PostTitle>
-      <PostContent>{content}</PostContent>
-      <BtnBox>
+      <UserBlock>
+        <div className="user-box">
+          <div className="profile"></div>
+          <div>{user_id}</div>
+        </div>
         <Button
           text="일정보기"
           onClick={() =>
             navigate(`/plan/${plan_id}`, { state: { plan_id: plan_id } })
           }
         />
-      </BtnBox>
+      </UserBlock>
+      <PostContent dangerouslySetInnerHTML={{ __html: content }}></PostContent>
+      <BtnBox></BtnBox>
     </PostPageBlock>
   );
 };
 
 const PostPageBlock = styled.main``;
 
+const UserBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  border-bottom: 1px solid lightgray;
+
+  .user-box {
+    display: flex;
+    align-items: center;
+    .profile {
+      border-radius: 50%;
+      background-color: gray;
+      width: 30px;
+      height: 30px;
+      margin-right: 10px;
+    }
+  }
+`;
+
 const PostTitle = styled.div`
   font-size: 2rem;
   font-weight: bold;
   padding: 10px;
+
+  border-bottom: 1px solid lightgray;
 `;
 
 const PostContent = styled.div`
   padding: 10px;
+  min-height: 400px;
+  box-sizing: border-box;
+
+  h1 {
+    font-size: 2em;
+  }
+
+  h2 {
+    font-size: 1.8em;
+  }
+
+  h3 {
+    font-size: 1.6em;
+  }
 `;
 
 const BtnBox = styled.div`
