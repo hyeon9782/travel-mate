ab87de99c82cc30480e8898393d01274b867b339
hyeon9782 <hyeon9782@gmail.com>
2023-08-20 23:04:00 +0900

    feat: LoginDialog 개발

---
+++ src/components/dialog/LoginDialog.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dialog from "./Dialog";
+
+const LoginDialog = () => {
+  return (
+    <Dialog>
+      <div>로그인을 해주세요</div>
+    </Dialog>
+  );
+};
+
+export default LoginDialog;
