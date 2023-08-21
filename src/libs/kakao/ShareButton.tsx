6807d415b0a22c594b43a1bd3e04f8dbd966d436
hyeon9782 <hyeon9782@gmail.com>
2023-08-21 18:48:18 +0900

    feat: 카카오톡 공유하기 기능 개발

---
+++ src/libs/kakao/ShareButton.tsx
@@ -0,0 +1,59 @@
+import { useEffect } from "react";
+
+type Props = {
+  title: string;
+  description: string;
+  imageUrl: string;
+  buttonTitle: string;
+};
+
+const { Kakao }: any = window;
+
+const KAKAO_API_KEY = import.meta.env.VITE_APP_KAKAO_API_KEY;
+const ShareButton = ({ title, description, imageUrl, buttonTitle }: Props) => {
+  const realUrl = "https://travel-mate-eta.vercel.app/";
+  const resultUrl = window.location.href;
+  useEffect(() => {
+    Kakao.cleanup();
+    Kakao.init(KAKAO_API_KEY);
+    console.log(Kakao.isInitialized());
+  }, []);
+
+  function shareMessage() {
+    Kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title,
+        description,
+        imageUrl,
+        link: {
+          // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
+          mobileWebUrl: realUrl,
+          webUrl: realUrl,
+        },
+      },
+      social: {
+        likeCount: 286,
+        commentCount: 45,
+        sharedCount: 845,
+      },
+      buttons: [
+        {
+          title: buttonTitle,
+          link: {
+            mobileWebUrl: realUrl,
+            webUrl: realUrl,
+          },
+        },
+      ],
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={() => shareMessage()}>카카오톡 공유하기</button>
+    </div>
+  );
+};
+
+export default ShareButton;
