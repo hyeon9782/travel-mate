964186a727ac6f83774b6a201bfeb08bdd9533ad
hyeon9782 <hyeon9782@gmail.com>
2023-10-22 20:46:58 +0900

    remove: react quert 패키지 및 관련 코드 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import GlobalStyle from "./styles/GlobalStyles.tsx";
 import React from "react";
 import DialogsProvider from "./provider/DialogsProvider.tsx";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false, // 윈도우가 다시 포커스되었을때 데이터를 호출할 것인지
-      retry: 0, // API 요청 실패시 재시도 하는 옵션 (설정값 만큼 재시도)
-    },
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <DialogsProvider>
-        <GlobalStyle />
-        <App />
-      </DialogsProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <DialogsProvider>
+      <GlobalStyle />
+      <App />
+    </DialogsProvider>
   </React.StrictMode>
 );
